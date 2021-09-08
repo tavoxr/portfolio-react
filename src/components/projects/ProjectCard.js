77935b3c670b96f7b0218b2d82a8d43b833dcc4a
tavoxr <tavoxr23@gmail.com>
2021-09-08 11:26:42 -0600

    card image fixes

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+//import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -82,7 +82,7 @@ function ProjectCard(props) {
               title="Contemplative Reptile"
             />
             */}
-            <img  src={props.imageUrl} className={classes.imgCard}/>
+            <img  src={props.imageUrl} className={classes.imgCard} alt="project-img"/>
             <CardContent>
               <Typography variant="h5" component="h5" className={classes.textCard}>
                 <span className={classes.projectTitle}>{props.title}</span>
